26e235b78ca46ac2ca223c0398bce85e2ea3bdad
Joao Paulo Costa de Araujo <jpaulo.caraujo@gmail.com>
2023-05-17 15:53:34 +0200

    Setting up a logger

--- src/experimentManager.py
+++ src/experimentManager.py
@@ -1,10 +1,15 @@
-import carla
 import random
+import logging
+import carla
 
 class ExperimentManager():
     def __init__(self, config, *args, **kwargs):
         self.client = carla.Client(config.HOST, config.PORT)
         self.client.set_timeout(config.CLIENT_TIMEOUT)
+
+        logging.info(f"Setting Client Host: {config.HOST}")
+        logging.info(f"Setting Client Port: {config.PORT}")
+        logging.info(f"Setting Client Client Timeout: {config.CLIENT_TIMEOUT}")
 
         self.world = self.client.get_world()
         self.spectator = self.world.get_spectator()
@@ -18,13 +23,24 @@ class ExperimentManager():
         vehicle = self.world.spawn_actor(vehicle_bp, transform)
         self.actor_list.append(vehicle)
 
+        logging.info('created %s' % vehicle.type_id)
+
         if autopilot:
             vehicle.set_autopilot(True)
         return vehicle, vehicle_bp, transform
 
     def tear_down(self):
-        print('destroying actors')
+        logging.info(f"Destroying actors.")
+        
         for sensor in self.sensors:
-            sensor.destroy()
-        self.client.apply_batch([carla.command.DestroyActor(x) for x in self.actor_list])
+            try:
+                sensor.destroy()
+            except Exception as e:
+                logging.error("Error at %", 'destroying', exc_info=e)
+
+        try:
+            self.client.apply_batch([carla.command.DestroyActor(x) for x in self.actor_list])
+        except Exception as e:
+            logging.error("Error at %", 'destroying', exc_info=e)
+
         print('done.')
