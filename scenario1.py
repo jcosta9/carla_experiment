03901a00fa9992321f87a5095be6fd54c2e6fc9b
Eik Reichmann <eik.reichmann@gmail.com>
2023-05-17 13:04:52 +0200

    adding segmentation sensor

--- scenario1.py
+++ scenario1.py
@@ -11,6 +11,7 @@ def main():
         spectator = world.get_spectator()
         blueprint_library = world.get_blueprint_library()
         actor_list = []
+        sensors = []
 
         vehicle_bp = random.choice(blueprint_library.filter('vehicle'))
 
@@ -19,20 +20,34 @@ def main():
         actor_list.append(vehicle)
         vehicle.set_autopilot(True)
 
+        spectator.set_transform(transform)
+
         camera_bp = blueprint_library.find('sensor.camera.rgb')
         camera_transform = carla.Transform(carla.Location(x=1.5, z=2.4))
         camera_rgb = world.spawn_actor(camera_bp, camera_transform, attach_to=vehicle)
         camera_rgb.listen(lambda image: image.save_to_disk('../_out/%06d.png' % image.frame))
         actor_list.append(camera_rgb)
+        sensors.append(camera_rgb)
 
-        spectator.set_transform(camera_rgb.get_transform())
+        sem_bp = world.get_blueprint_library().find('sensor.camera.semantic_segmentation')
+        sem_bp.set_attribute("image_size_x",str(1920))
+        sem_bp.set_attribute("image_size_y",str(1080))
+        sem_bp.set_attribute("fov",str(105))
+        sem_location = carla.Location(2,0,1)
+        sem_rotation = carla.Rotation(0,0,0)
+        sem_transform = carla.Transform(sem_location,sem_rotation)
+        sem_cam = world.spawn_actor(sem_bp,sem_transform,attach_to=vehicle, attachment_type=carla.AttachmentType.Rigid)
+        # This time, a color converter is applied to the image, to get the semantic segmentation view
+        sem_cam.listen(lambda image: image.save_to_disk('../tutorial/new_sem_output/%.6d.jpg' % image.frame,carla.ColorConverter.CityScapesPalette))
 
-        time.sleep(15)
+
+        time.sleep(5)
 
 
     finally:
         print('destroying actors')
-        camera_rgb.destroy()
+        for sensor in sensors:
+            sensor.destroy()
         client.apply_batch([carla.command.DestroyActor(x) for x in actor_list])
         print('done.')
 
