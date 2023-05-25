1283743cea33a82d29c68f45d384b13105ac198e
Joao Paulo Costa de Araujo <jpaulo.caraujo@gmail.com>
2023-05-25 19:05:17 +0200

    Adding functions to label a scene
    According to the documentation, the semantic segmentation camera
    provides the labels of each object in the scene as a tag in the red
    channel. The code extracts the red channel, maps the labels with their
    respective values and saves everything under the DATA_OUT folder.
    
    Three facts are noteworthy. First is that these functios should soon be
    moved to a class that handles only aspects related to cameras. Second,
    the dictionary that maps the values in the red channel with the tags was
    extracted from the source code, and not from the documentation. The
    latest version of the documentation ranges the values from 0 to 22, and
    often we were getting higher values. By searching the source code from
    the cpp carla's lib, we found the mapping that is performed behind the
    scenes. This mapping was the one added.  Third, we had some trouble
    when verifying if the labels assigned were correct. We haven't put much
    effort to it, though.

--- src/experimentManager.py
+++ src/experimentManager.py
@@ -1,6 +1,85 @@
+import os
 import random
 import logging
+from datetime import date
+import numpy as np
 import carla
+
+############################################
+# Add this to a sensor handler class
+############################################
+'''
+The labels below match the ones found in the cpp file carla/LibCarla/source/carla/rpc/ObjectLabel.h
+We belive that the documentation hasn't been updated, since it ranges only from 0 to 22, and larger 
+tag values were found. Besides, the order changes a bit.
+'''
+DETECTED_TAGS = {
+    
+    0 : "None",
+    1 : "Roads",
+    2 : "Sidewalks",
+    3 : "Buildings",
+    4 : "Walls",
+    5 : "Fences",
+    6 : "Poles",
+    7 : "TrafficLight",
+    8 : "TrafficSigns",
+    9 : "Vegetation",
+    10 : "Terrain",
+    11 : "Sky",
+    12 : "Pedestrians",
+    13 : "Rider",
+    14 : "Car",
+    15 : "Truck",
+    16 : "Bus",
+    17 : "Train",
+    18 : "Motorcycle",
+    19 : "Bicycle",
+    20 : "Static",
+    21 : "Dynamic",
+    22 : "Other",
+    23 : "Water",
+    24 : "RoadLines",
+    25 : "Ground",
+    26 : "Bridge",
+    27 : "RailTrack",
+    28 : "GuardRail",
+}
+
+def to_bgra_array(image):
+    array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
+    array = np.reshape(array, (image.height, image.width, 4))
+    return array
+
+def labels_to_array(image):
+    return to_bgra_array(image)[:, :, 2]
+
+def get_image_contents(image, config) -> np.array:
+   today = date.today().strftime('%Y%m%d')
+   frame = '%.6d' % image.frame
+
+   semseg_raw_path = os.path.join(*config.DATA_OUTPUT, 'semseg_raw')
+   if not os.path.exists(semseg_raw_path):
+       os.makedirs(semseg_raw_path)
+
+   semseg_labels_path = os.path.join(*config.DATA_OUTPUT, 'semseg_labels')
+   if not os.path.exists(semseg_labels_path):
+       os.makedirs(semseg_labels_path)
+
+   red_channel = labels_to_array(image)
+   red_channel_txt = np.vectorize(DETECTED_TAGS.get)(red_channel)
+   unique_ids = np.unique(red_channel)
+   unique_labels = np.vectorize(DETECTED_TAGS.get)(unique_ids)
+
+   image.save_to_disk(os.path.join(semseg_raw_path, f'{today}_{frame}.jpg'))
+   image.save_to_disk(os.path.join(semseg_raw_path, f'{today}_{frame}_colored.jpg'), carla.ColorConverter.CityScapesPalette)
+
+   red_channel.tofile(os.path.join(semseg_raw_path, f'{today}_{frame}_redchannel.csv'), sep = ',')
+   red_channel_txt.tofile(os.path.join(semseg_labels_path, f'{today}_{frame}_rawlabels.csv'), sep = ',')
+   unique_ids.tofile(os.path.join(semseg_labels_path, f'{today}_{frame}_unique.csv'), sep = ',')
+   unique_labels.tofile(os.path.join(semseg_labels_path, f'{today}_{frame}_unique_labels.csv'), sep = ',')
+  
+
 
 class ExperimentManager():
     def __init__(self, config, *args, **kwargs):
